8101e33bc50cf762f3b73fe7d6a54bf8a9ad18e8
lubyluby <lubyluby@126.com>
2018-09-14 16:53:42 +0800

    弹球游戏

--- ChildrenDemo/run.py
+++ ChildrenDemo/run.py
@@ -2,46 +2,46 @@
 import TurtlePaint
 import time
 import TkPaint
+import TkBall
+import TkPanddle
 from tkinter import*
 
 
 if __name__=="__main__":
-    '''
-    turtle.color("red","yellow")
-    turtle.speed(10)
-    turtle.begin_fill()
-    #sunflower
-    #sunflow.Paint.sunFlower(200,170)
-    #circle
-    #sunflow.Paint.sunFlower(15,15)
-    #多边形
-    sunflow.Paint.sunFlower(200,70)
-    turtle.end_fill()
-    time.sleep(1)
-    '''
-    '''
-    turtle.setup(1500, 1400, 0, 0)
-    turtle.pensize(30)  # 画笔尺寸
-    turtle.pencolor("green")
-    turtle.speed(1)
-    turtle.seth(270)    # 前进的方向0east,90north
-    sunflow.Paint.drawSnake(70, 80,2, 15)
-    #多边形直线进行画
-    #turtle.circle(70,steps=4)
-    '''
-    
-    #sunflow.Paint.fiveAngle()
-    #turtle.color("red","yellow")
-    #turtle.speed(10)
-    #turtle.begin_fill()
     #wash=TurtlePaint.Paint()
     #wash.sunFlower()
     #wash.drawSnake()
     #wash.fiveAngle()
     #wash.heart()
     #wash.bus()
-    tkp=TkPaint.Paint()
-    tkp.input("mary")
+    #tkp=TkPaint.Paint()
+    #tkp.input("mary")
+    #tkp.createLine()
+    #tkp.createBox()
+    #tkp.createModernPaint("grey")
+    #tkp.arc(359) #359为整圆，360==0
+    #tkp.shape()
+    #tkp.animation()
+    #tkp.bindEvent()
+    #弹球
+    tk=Tk()
+    tk.title("Game")
+    tk.resizable(0,0)#窗口在垂直和水平方向都不能改变
+    tk.wm_attributes("-topmost",1)#画布窗口放在所有窗口之前
+    canvas=Canvas(tk,width=500,height=400,bd=0,highlightthickness=0)#确保画布之外没有边框
+    canvas.pack()
+    tk.update()
+    paddle=TkPanddle.Paddle(canvas,'green')
+    ball=TkBall.Ball(canvas,paddle,'red')
+    while 1:
+        if ball.hit_bottom==False:
+            ball.draw()
+            paddle.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
+    tk.mainloop()
+    
     
    
     
